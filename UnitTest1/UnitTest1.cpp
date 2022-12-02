878b08e0b801cb189fe76722a72eb2bb329c35bf
Tetiana Horiana <on>
2022-12-02 04:10:46 +0100

    Добавьте файлы проекта.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Asus\Desktop\1 курс\мпзз\мпзз\мпзз.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(calculate_Tests)
+    {
+    public:
+        TEST_METHOD(calculate6_and_5_21rtn)
+        {
+            int n = 6;
+            int x = 5;
+            int expec = 21;
+            int actual = y(n, x);
+            Assert::AreEqual(expec, actual);
+        }
+
+    public:
+        TEST_METHOD(calculate8_and_10_78rtn)
+        {
+            int n = 8;
+            int x = 10;
+            int expec = 78;
+            int actual = y(n, x);
+            Assert::AreEqual(expec, actual);
+        }
+    };
+
+    TEST_CLASS(checkValidParams_Tests)
+    {
+    public:
+        TEST_METHOD(checkValidParams_minus100)
+        {
+            int n = -100;
+            int x = -100;
+            try
+            {
+                checkValidParams(n, x);
+                Assert::Fail();
+            }
+            catch (...) {
+                Assert::IsTrue(true);
+            }
+        }
+    };
+}
---
+++ ипз3/ипз3.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+using namespace std;
+
+void checkValidInput() {
+    setlocale(LC_ALL, "UKRAINIAN");
+    if (cin.fail()) {
+        throw "Невiрне введення";
+    }
+}
+
+void checkValidParams(int n, int x)
+{
+    setlocale(LC_ALL, "UKRAINIAN");
+    if (x <= 0)
+    {
+    if (n < 0) {
+            throw "Невiрне введення даних";
+        }
+    } 
+}
+
+int first(int n, int x)
+{
+    int p = 1;
+    for (int i = 0; i <= n; i++)
+    {
+        p *= i * i + i;
+    }
+    return (p);
+}
+
+int second(int n, int x)
+{
+    int sum = 0, sum1 = 0;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int  sum1= 0;
+        for (int j = 0; j <= n - 1; j++)
+        {
+            sum1 += x / (i + j);
+        }
+
+     
+        sum += sum1;
+    }
+    return (sum);
+}
+
+int y(int n, int x)
+{
+    setlocale(LC_ALL, "UKRAINIAN");
+    if (x < 0)
+    {
+        if (n < 0) {
+            cout << "Неправильнi данi";
+            exit;
+        }
+        return (first(n, x));
+    }
+    else
+    {
+        if (n < 0) {
+            cout << "Неправильнi данi";
+            exit;
+        }
+        return (second(n, x));
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "UKRAINIAN");
+    int n, x = { 0 };
+    try {
+        cout << "Введiть n & x: ";
+        cin >> n >> x;
+        checkValidParams(n, x);
+        checkValidInput();
+        cout << "y = " << y(n, x);
+    }
+    catch (const char* ex) {
+        cout << ex << endl;
+        return -1;
+    }
+    catch (...) {
+        cout << "Не вiдома помилка";
+    }
+}
